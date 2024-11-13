845ba5034afee0fdf782117fd8557e5d82b81645
Noah Morse <noahmorse14@gmail.com>
2024-11-12 23:26:15 -0500

    updating with jwt

--- 05-apis/abuseipdb.py
+++ 05-apis/abuseipdb.py
@@ -16,11 +16,23 @@ import requests
 from pprint import pprint
 import json
 
-payload = {"ipAddress": "241.163.94.187"}
 url = "https://api.abuseipdb.com/api/v2/check/"
 headers = {
   'Key': '2da6b3b57dbffdf98880bad799338b21328eb9077ce8aeb1d7d2fc32e87c7cea21fa5af9caea1d7c'
 }
-
 ip_dictionary = {}
 ip_list = []
+
+
+# Open the JSON log file
+
+
+# Append the IP Addresses from the log file to the list
+
+
+for ip in ip_list:
+    payload = {"ipAddress": ip}
+
+    # Finish the for loop by making the request, checking the abusescore, and adding the IP to the dictionary if it is flagged
+
+
